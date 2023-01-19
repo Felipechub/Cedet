665dad7bff2c7355d842476d6647f2069c57eca5
Felipe Carcamo <felipecarcamojara@gmail.com>
2023-01-19 01:52:22 -0300

    Se agrega filtro de provincia y un grafico

--- analisis.py
+++ analisis.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import plotly.express as px
 
-a = pd.read_excel("Campamentos_2019.xlsx")
-print(a)
+df_campamentos = pd.read_excel("Campamentos_2019.xlsx")
+
+df_filtrado = df_campamentos[df_campamentos["region"] == "Biobío"]
+df_arauco = df_filtrado[df_filtrado['provincia'] == "Arauco"]
+
+
+val_p4 = df_arauco['p4'].value_counts().index
+cant_p4 = list(df_arauco['p4'].value_counts())
+fig = px.bar(df_arauco, x=val_p4, y=cant_p4) 
+fig.show()
